b04506277c7a99cfb5423a8461e3c2050a5fd6d2
StefanRaluca <raluca.bubulina@yahoo.com>
2024-02-16 15:34:57 +0100

    add exercise 2c

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,12 +12,34 @@ let number = 100;
 console.log(number);
 
 
-//Esercizio 2
+//Esercizio 2 a)
 
 
 /*1.Dichiaro una constante con il nome PI alla quale gli assegno un varole di  3.14
 2.Stampo nella console usando il console log con il richiamo del nome della constante
  */
 
-const PI = 3.14;
+/*const PI = 3.14;
+console.log(PI);*/
+
+
+//Esercizio 2 B)
+/*. 1.Provo ad assegnare alla constante PI il valore di 3.1416
+2. Per cambiare il valore della constante , dovrei annulare la prima constante dichiarata (vedi es 2 a),altrimenti la console segnala un errore  dove ci dice che il valore della constante PI è già assegnato. 
+ */
+
+const PI =3.1416;
 console.log(PI);
+
+
+//Esercizio 2 C)
+/*1.Dichiaro una variabile con il nome radius e assegno il numero 8.
+2.Faccio una operazione fra la Variabile PI e la variabile radius
+3.Assegno ad una variabile nome circle e faccio l'operazione come da richiesta utilizzando il nome delle variabili in precendenza
+4. Stampo il nome della variabile circle nella console.
+ */
+
+
+let radius =8;
+let circle = (radius *2 * PI)
+console.log(circle);
