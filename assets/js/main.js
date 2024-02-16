a28e3445c2e23239fbce4e69f528b320fec22d1c
StefanRaluca <raluca.bubulina@yahoo.com>
2024-02-16 15:09:39 +0100

    add exercise 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,17 @@
  2. Lo stampo nella console usando il console log richiamando il nome della variabile  
  */
 
-
-
+ 
 let number = 100;
 console.log(number);
+
+
+//Esercizio 2
+
+
+/*1.Dichiaro una constante con il nome PI alla quale gli assegno un varole di  3.14
+2.Stampo nella console usando il console log con il richiamo del nome della constante
+ */
+
+const PI = 3.14;
+console.log(PI);
