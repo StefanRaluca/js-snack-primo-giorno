00dd833315436740f5291afd3885439029ef9c55
StefanRaluca <raluca.bubulina@yahoo.com>
2024-02-16 14:59:41 +0100

    add exercise 1

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+
+// Esercizio 1
+
+/*1.Dichiaro una variabile con il nome number usando let/const
+
+
+ 2. Lo stampo nella console usando il console log richiamando il nome della variabile  
+ */
+
+
+
+let number = 100;
+console.log(number);
